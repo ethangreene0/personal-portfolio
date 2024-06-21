e08751d278a77d060952b31a55f9d7c6a3ba20ae
ethangreene0 <egreene_11@icloud.com>
2024-06-20 22:21:46 -0400

    Signed-off-by: ethangreene0 <egreene_11@icloud.com>

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/ethan-greene/" target="_blank" rel="noopener noreferrer">
+                <img src={navIcon1} alt="LinkedIn Icon" />
+              </a>
+              <a href="https://www.instagram.com/egreene_11/" target="_blank" rel="noopener noreferrer">
+                <img src={navIcon2} alt="Facebook Icon" />
+              </a>
+              <a href="https://github.com/ethangreene0" target="_blank" rel="noopener noreferrer">
+                <img src={navIcon3} alt="Instagram Icon" />
+              </a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,108 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import resumeImg from "../assets/img/resume-img.jpg";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Image Scaler",
+      description: "Java image compression program using Quadrant Trees with GUI for resolution changes and pixel analysis.",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Calculator App/Equation's",
+      description: "Created an Android application using Java and Android Studio with java script implementations",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  const Files = [
+    {
+        title: "Resume",
+        imgUrl: resumeImg,
+        fileUrl: require("../assets/Files/Resume.pdf"), 
+    },
+  ];
+
+  return (
+    <section className="project" id="about me">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>About Me</h2>
+                <p>Place Holder.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Projects</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Resume</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                            {
+                            Files.map((file, index) => {
+                                return (
+                                <Col key={index} sm={6} md={4}>
+                                  <div className="proj-imgbx">
+                                    <img src={file.imgUrl} alt={file.title}/>
+                                    <div className="proj-txtx">
+                                      <h4>{file.title}</h4>
+                                      <a href={file.fileUrl} download>Download</a>
+                                    </div>
+                                  </div>
+                                </Col>
+                                )
+                            })
+                            }
+                        </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>place holder</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
